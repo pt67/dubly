aa205229ea255f9abfae6b53fe77dbad89c549c5
prakash <pt6769759@gmail.com>
2022-06-09 13:28:46 -0400

    makit beautiful

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,16 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
+
+const [data, setData] = useState();
+
+useEffect(()=>{
+ 
+var dat = JSON.parse(localStorage.getItem('items'));
+//console.log(JSON.parse(dat)[0].name);
+setData(dat);
+ 
+ 
+ }, []);
+ 
+console.log(data);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Added Items</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <div>
+       <ul className="list-group">
+        
+        {data?.map(e=>
+        <li className="list-group-item" key={e.name}>{ e.name }</li>
+        )}
+        
+       </ul>
+      </div>
     </View>
   );
+  
+  
+  
 }
 
 const styles = StyleSheet.create({
